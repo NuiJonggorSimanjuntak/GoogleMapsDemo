afbb2b932e9bb2cd63b9f775af664269d6ccb7bf
nuijs <nui.j.simanjuntak29@gmail.com>
2022-06-13 09:13:02 +0700

    Tugas 1

---
+++ app/src/main/java/com/example/googlemapsdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.googlemapsdemo;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Pamulang = new LatLng(-6.355778, 106.731148);
+        map.addMarker(new MarkerOptions().position(Pamulang).title("Pamulang"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Pamulang));
+    }
+}
